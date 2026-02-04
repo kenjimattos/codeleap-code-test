e720d651c3913ac1e16b382bb8cee9c5f630a3bd
kenjimattos <70449934+kenjimattos@users.noreply.github.com>
2026-02-04 14:56:32 -0300

    feat: Create Main component with initial layout and styles

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { useUser } from '../../context/UserContext';
+import styles from './Main.module.css';
+
+function Main() {
+  
+  return (
+    <div className={styles.page}>
+     
+      <main className={styles.content}>
+       
+      </main>
+
+    </div>
+  );
+}
+
+export default Main;
