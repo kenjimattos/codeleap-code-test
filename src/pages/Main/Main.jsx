52c7a2d7d26716cf5e4a189aa47bc15718db30fe
kenjimattos <70449934+kenjimattos@users.noreply.github.com>
2026-02-04 15:02:54 -0300

    feat: Add Header component with styles and integrate into Main page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { useUser } from '../../context/UserContext';
 import styles from './Main.module.css';
+import Header from '../../components/posts/Header';
 
 function Main() {
   
   return (
     <div className={styles.page}>
-     
+     <Header />
       <main className={styles.content}>
        
       </main>
